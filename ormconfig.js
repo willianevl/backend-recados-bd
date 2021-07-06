e88136c16794eb6cb2de17073cf5fc84ae32c938
Willian Guimarães <willianevalt@gmail.com>
2021-07-06 19:29:15 -0300

    mudanças para produção

--- ormconfig.js
+++ ormconfig.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 module.exports = {
     type: 'postgres',
-    // url: process.env.DATABASE_URL,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
+    url: process.env.DATABASE_URL,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // username: process.env.DB_USERNAME,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DATABASE,
     synchronize: false,
     logging: false,
     extra: {
